25b4d4b373720ebf6d4f77bd3946a94e1f478333
saitoumizuki <yoshihiramizuki@gmail.com>
2018-05-31 14:32:47 +0900

    Finish7-6

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
-// $(function(){
-//   $('.box1').slideDown(function(){
-//         $('.box1').css({
-//             'background-color': '#0000FF',
-//             'width': '200px',
-//             'height': '100px'
-//         }).slideUp();
-//     });
-// });
-
 $(function(){
-    $('.box1').show(10000);
-    $('.box1').css({'background-color': '#0000FF'});
+    $('.box1').on('click', function(){
+        $('.box1').addClass('box1-ext');
+    });
+
+    $('.box1').mouseout(function(){
+        $('.box1').removeClass('box1-ext');
+    });
+
 });
